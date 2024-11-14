b99515e8b15b26396e7db7317ee2bef80d767235
chaim <shterenchaim@gmail.com>
2024-11-14 13:34:13 +0200

    change sentence to produce one

--- message_producer/app/service/email_service.py
+++ message_producer/app/service/email_service.py
@@ -3,14 +3,15 @@ from message_producer.app.service.uttils import check_terror_message
 
 
 def sorting_emails(email):
+    email_producer.produce_all_messages(email)
     sentences = email["sentences"]
     for sentence in sentences:
         checked_message = check_terror_message(sentence)
         if checked_message is "explosive":
+            email["sentences"] = sentence
             email_producer.produce_explosive_messages(email)
-            return
         if checked_message is "hostage":
+            email["sentences"] = sentence
             email_producer.produce_hostage_messages(email)
-            return
-        else:
-            email_producer.produce_all_messages(email)
+
+
