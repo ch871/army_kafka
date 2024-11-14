e5fc32707eba51fbf70ee752e7f330bd7b71b78c
chaim <shterenchaim@gmail.com>
2024-11-14 13:41:03 +0200

    append topics and produce

--- message_producer/app/kafka_producer/email_producer.py
+++ message_producer/app/kafka_producer/email_producer.py
@@ -4,9 +4,23 @@ import os
 
 load_dotenv(verbose=True)
 all_messages_topic = os.environ['ALL_MESSAGES_TOPIC']
+explosive_topik =os.environ["EXPLOSIVE_TOPIC"]
+hostage_topic=os.environ["HOSTAGE_TOPIC"]
 
 
 def produce_all_messages(message_info):
     produce(
         topic=all_messages_topic,
+        value=message_info)
+
+
+def produce_explosive_messages(message_info):
+    produce(
+        topic=explosive_topik,
+        value=message_info)
+
+
+def produce_hostage_messages(message_info):
+    produce(
+        topic=hostage_topic,
         value=message_info)
